e7fbb8f85aaa537b48f73b2a12caf820263251a7
GonzaCostanzi <gncostanzi@gmail.com>
2023-01-02 20:55:41 -0300

    modifica para deploy

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app= express();
 
 app.use(express.static(path.join(__dirname, 'public')) );
 
-app.listen(3000, ()=> console.log('Servidor corriendo'));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, ()=> console.log(`Servidor corriendo en el puerto ${PORT}`));
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'views/home.html'));
